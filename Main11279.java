44cd89f23ec7c302b91280aeb192afa546f98435
lovinix <sylph0606@gmail.com>
2019-04-15 10:18:06 +0000

    BOJ 11279

---
+++ Main11279.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main11279
+{
+    static PriorityQueue<Integer> q;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        q = new PriorityQueue<>((o1,o2)->o2-o1);
+        int N = Integer.parseInt(br.readLine());
+        while(N-->0)
+        {
+            int x = Integer.parseInt(br.readLine());
+            if(x==0)
+            {
+                if(q.isEmpty()) bw.write("0\n");
+                else bw.write(q.poll()+"\n");
+            }
+            else
+            {
+                q.offer(x);
+            }
+        }
+        bw.close();
+    }
+}
