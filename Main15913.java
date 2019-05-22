2de0ed0663a48d627713c6aab8cb3b55cc2fef60
lovinix <sylph0606@gmail.com>
2019-05-22 17:20:04 +0000

    BOJ 15913

---
+++ Main15913.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main15913
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("1");
+        bw.close();
+    }
+}
