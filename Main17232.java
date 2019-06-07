21c760dbffbd2d4279a51d2dbfaf9d92a72a665a
lovinix <sylph0606@gmail.com>
2019-06-07 08:36:12 +0000

    BOJ 17232

---
+++ Main17232.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main17232
+{
+    static int N,M,K,a,b;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.close();
+    }
+
+}
+class Life
+{
+    boolean flag;
+    int num;
+    Life(boolean flag)
+    {
+        this.flag = flag;
+    }
+
+}
